1364f5a142ad60697eabf68ad3a02cf537080597
Putu Widyantara <putuwaw973@gmail.com>
2022-04-28 12:31:27 +0800

    Initial commit

---
+++ lib/sample_lib.c
@@ -0,0 +1,4 @@
+#include "sample_lib.h"
+void printHelloWorld(){
+    printf("Hello World");
+}
---
+++ lib/sample_lib.h
@@ -0,0 +1,7 @@
+#ifndef SAMPLE_LIB_H_INCLUDED
+#define SAMPLE_LIB_H_INCLUDED
+
+#include <stdio.h>
+void printHelloWorld();
+
+#endif // SAMPLE_LIB_H_INCLUDED
