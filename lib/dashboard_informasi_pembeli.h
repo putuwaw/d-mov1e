fab7a1b66d95692767c7084919d8e49a2550915d
Krisna Wandhana <102802614+krisnawandhana@users.noreply.github.com>
2022-05-28 15:40:16 +0800

    Update dashboard_informasi_pembeli.h

--- lib/dashboard_informasi_pembeli.h
+++ lib/dashboard_informasi_pembeli.h
@@ -3,4 +3,7 @@
 
 #include "dashboard_informasi.h"
 
+void display_informasi_pembeli();
+void handle_display_informasi_pembeli(GtkWidget *widget, GtkWidget *window);
+
 #endif // DASHBOARD_INFORMASI_PEMBELI_H_INCLUDED
